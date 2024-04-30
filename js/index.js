aae9b4ea6166db2cfdbd0d279ccd2e88378088e4
GINTOAHC <chaotingchen10@gmail.com>
2024-05-01 01:39:14 +0800

    feat: Fetch data instead of hard-coding

--- js/index.js
+++ js/index.js
@@ -1,37 +1,20 @@
+// Repo URL
 const repo = 'github/gnitoahc/slides/blob/main/'
-// Files
-let files = [
-  {
-    name: 'TF-IDF',
-    date: '2024-04-02',
-    desc: 'Intro to TF-IDF - course 1122 GenAI',
-    tags: ['GenAI', 'NLP'],
-    path: `${repo}TF-IDF.pdf`,
-  },
-  {
-    name: '1122CPPy-Lab02',
-    date: '2024-03-05',
-    desc: 'Lab02 of course Computer Programming, NCCU',
-    tags: ['NCCU', 'Programming', 'CPPY'],
-    path: `${repo}1122CPPy-Lab02.pdf`,
-  },
-  {
-    name: 'DBMS-2023',
-    date: '2023-01-01',
-    desc: 'Description',
-    tags: ['NCCU', 'Database'],
-    path: 'github/gnitoahc/DBMS-2023/blob/main/pdf/%E9%A1%8C%E7%9B%AE%E8%AA%AA%E6%98%8E.pdf',
-  },
-]
-
+// Data URL
+const rawFile =
+  'https://raw.githubusercontent.com/GNITOAHC/slides/main/js/files.json'
 // Prefix for the href (to display PDF via nbviewer.org)
-const hrefPrefix = 'https://nbviewer.org/'
+const pdfPrefix = 'https://nbviewer.org/'
+
+async function fetchFiles() {
+  const response = await fetch(rawFile)
+  return await response.json()
+}
 
 // Sort files by date (newest first)
 function sortFunc(a, b) {
   return a.date < b.date ? 1 : -1
 }
-files = files.sort((a, b) => sortFunc(a, b))
 
 function getDate(date) {
   return new Date(date).toLocaleDateString('en-US', {
@@ -51,7 +34,7 @@ function listComponent(file) {
     <div class="relative p-5 rounded w-full hover:bg-white/10">
       <div class="flex w-full relative">
         <a class="text-white" href="#${file.name}">${file.name}</a>
-        &nbsp;<a href="${hrefPrefix}${file.path}" class="h-3 w-3"><link-icon class="h-3 w-3"></link-icon></a>
+        &nbsp;<a href="${pdfPrefix}${file.path}" class="h-3 w-3"><link-icon class="h-3 w-3"></link-icon></a>
         <p class="text-white absolute right-2">${getDate(file.date)}</p>
       </div>
       <p class="opacity-75">${file.desc}</p>
@@ -61,8 +44,24 @@ function listComponent(file) {
   `
 }
 
-document.getElementById('slides').innerHTML = files
-  .map((file) => {
-    return listComponent(file)
-  })
-  .join('')
+function setDoc(files) {
+  document.getElementById('slides').innerHTML = files
+    .map((file) => {
+      return listComponent(file)
+    })
+    .join('')
+}
+
+async function main() {
+  // Retrieve files from JSON
+  let files = await fetchFiles()
+
+  // Preprocess files
+  files = files.files // Set files to the JSON's files object
+  files.forEach((file) => (file.path = file.path.replace('${repo}', repo))) // Replace ${repo} with the actual repo
+  files = files.sort((a, b) => sortFunc(a, b)) // Sort files by date
+
+  // Set the document
+  setDoc(files)
+}
+main()
